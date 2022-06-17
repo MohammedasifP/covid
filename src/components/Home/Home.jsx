d57981151388f51817597eb5927b33ce6f740300
Mohammedasif Pinitod <87132170+MohammedasifP@users.noreply.github.com>
2022-06-17 15:28:03 +0530

    added sorting functionality

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,20 +1,75 @@
 import { useEffect, useState } from "react"
 import './Home.css'
-
+import {FaSortAmountDown} from "react-icons/fa";
+import {FaSortAmountDownAlt} from "react-icons/fa";
+import {FaSort} from "react-icons/fa";
 
 export const Home=()=>{
 const [covidata,setCoviddata]=useState({});
 const [countrydata,setCountrydata]=useState([])
+const [icon,setIcon]=useState(true)
+const [totalcases,setTotalcases]=useState(true);
+const [totaldeaths,setTotaldeaths]=useState(true);
+
 const [date,setDate]=useState({date:""})
     useEffect(()=>{
         fetch("https://asifcovidapp.herokuapp.com/getdata").then(Response=>Response.json()).then(data=>{setCoviddata(data.Global);setCountrydata(data.Countries)})
     },[])
-console.log(covidata.NewConfirmed)
+
     useEffect(()=>{
         const d=new Date().toLocaleString();
         setDate({...date,date:d})
     },[])
-  console.log(countrydata)
+
+   const countrySort=()=>{
+      setIcon(!icon)
+      if(icon){
+        const cdata=countrydata.sort((a, b) => b.Country.localeCompare(a.Country))
+        let data=[];
+        Object.assign(data,cdata)
+        setCountrydata(data)
+      }
+    else{
+        const cdata=countrydata.sort((a, b) => a.Country.localeCompare(b.Country))
+        let data=[];
+        Object.assign(data,cdata)
+        setCountrydata(data)
+    }
+   }
+
+   const totalSort=()=>{
+    setTotalcases(!totalcases)
+   if(totalcases){
+    const Tdata=countrydata.sort((a,b)=>{return a.TotalConfirmed-b.TotalConfirmed})
+    let data=[];
+    Object.assign(data,Tdata)
+    setCountrydata(data)
+   }
+   else{
+    const Tdata=countrydata.sort((a,b)=>{return -a.TotalConfirmed+b.TotalConfirmed})
+    let data=[];
+    Object.assign(data,Tdata)
+    setCountrydata(data)
+   }
+   }
+
+   const deathSort=()=>{
+    setTotaldeaths(!totaldeaths)
+    if(totaldeaths){
+     const Tdata=countrydata.sort((a,b)=>{return a.TotalDeaths-b.TotalDeaths})
+     let data=[];
+     Object.assign(data,Tdata)
+     setCountrydata(data)
+    }
+    else{
+     const Tdata=countrydata.sort((a,b)=>{return -a.TotalDeaths+b.TotalDeaths})
+     let data=[];
+     Object.assign(data,Tdata)
+     setCountrydata(data)
+    }
+        
+   }
+
     return (
         <div className="home">
            <div className="corona-top-heading-div">
@@ -37,13 +92,13 @@ console.log(covidata.NewConfirmed)
              </div> 
            </div>
            <div className="country-records-conatiner">
-             <table className="all-country-table" border="1px" cellSpacing="0" cellPadding="20px">
+             <table className="all-countries-table" border="1px" cellSpacing="0" cellPadding="10%">
                 <thead>
                    <tr>
-                    <th>Country</th>
-                    <th>Total Cases</th>
+                    <th onClick={countrySort} className="country-sort">Country {icon?<FaSortAmountDownAlt/>:<FaSortAmountDown/>}</th>
+                    <th onClick={totalSort} className="sort">Total Cases <FaSort/></th>
                     <th>New Cases</th>
-                    <th>Total Deaths</th>
+                    <th onClick={deathSort} className="sort">Total Deaths <FaSort/></th>
                     <th>New Deaths</th>
                     <th>Total Recovered</th>
                     <th>New Recovered</th>
